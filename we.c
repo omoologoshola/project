acf0d0f396d1ed941955d58b48bcb6f48a43b0f5
Aladeyelu Abdullahi <aladeyeluabdullahi@gmail.com>
2022-10-17 21:37:58 +0100

    multiply

---
+++ we.c
@@ -0,0 +1,12 @@
+/**
+ * main- a function that multiply two numbers
+ * @a- first variable
+ * @b- second variable
+ * Return- result upon success
+ */
+
+int mul(int a, int b)
+        {int result;
+                result=a*b;
+                return result;
+        }
